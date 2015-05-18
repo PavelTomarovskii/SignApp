c176d8179b2ef9f59c0afb22792bf405aaebba9b
maxim.saraev <maxim.saraev@dataart.com>
2015-05-18 09:29:31 +0300

    request page

---
+++ SignApplication/Scripts/Application/Request/Controller/requests.controller.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('request.module')
+        .factory('requestsController', requestsController);
+
+    requestsController.$inject = ['$http', '$log'];
+
+    function requestsController($http, $log) {
+
+        var vm = this;
+        vm.pageSize = 10;
+        vm.paginatorPages = 5;
+        vm.colModel = [];
+
+        vm.getSortOrder = getSortOrder;
+        vm.clickOnHeader = clickOnHeader;
+
+        activate();
+
+        function activate() {
+            
+            vm.colModel.push({ name: '#', predicate: 'id' });
+            vm.colModel.push({ name: 'Name', predicate: 'name' });
+            vm.colModel.push({ name: 'Grade', predicate: 'grade' });
+            vm.colModel.push({ name: 'Job', predicate: 'job' });
+            vm.colModel.push({ name: 'Age', predicate: 'age' });
+            
+        }
+        
+        function getSortOrder() {
+            return vm.reverse ? "glyphicon-sort-by-attributes-alt" : "glyphicon-sort-by-attributes";
+        }
+        
+        function clickOnHeader(predicate) {
+            vm.predicate = predicate;
+            vm.reverse = !vm.reverse;
+        }
+
+    }
+
+})();
